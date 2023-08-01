bee3d2c66ee1989d7c8fcf5d3d2da6cd07060250
Mishal Adnan <mishal.adnan26@gmail.com>
2023-08-01 20:10:29 +0500

    Updates

---
+++ precache-manifest.91431dea0a7b0c08456647008a254b8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0706bfdc8fd3d9e62ec",
+    "url": "/master/static/css/main.db024527.chunk.css"
+  },
+  {
+    "revision": "a0706bfdc8fd3d9e62ec",
+    "url": "/master/static/js/main.b6baf9a1.chunk.js"
+  },
+  {
+    "revision": "15014a7ea4c86c646f8e",
+    "url": "/master/static/js/runtime~main.835cc2c2.js"
+  },
+  {
+    "revision": "7200c67c60b40c291e08",
+    "url": "/master/static/js/2.83ef975b.chunk.js"
+  },
+  {
+    "revision": "50e2e84e62d3d028c80e",
+    "url": "/master/static/js/3.bb49b79f.chunk.js"
+  },
+  {
+    "revision": "a8f082f458dd120713b972029ac1131a",
+    "url": "/master/index.html"
+  }
+];
